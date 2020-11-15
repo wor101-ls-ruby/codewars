1a03fe33b574a5301f629a6fb81d73eec2c5eeb1
Will <wor101@gmail.com>
2020-11-15 17:51:11 -0500

    ore codewars practice

--- dice_probability.rb
+++ dice_probability.rb
@@ -1,72 +1,21 @@
-require 'pry'
+def dice_rolls(sum, dice)
+  final_rolls = []
 
-def dice_roll(sum, dice, rolls = Array.new(dice, 1))
-  desired_rolls = rolls
-
-  dice.times do |count|
-    (1..6).each do |roll|
-      other_rolls = [0]
-      if dice > 1
-        other_rolls =  dice_roll((sum - roll), (dice - 1)).flatten
-      end
-      current_sum = roll + other_rolls.sum
-      if current_sum == sum
-        good_rolls = []
-        good_rolls[0] = roll
-        other_rolls.each_with_index { |r, i| good_rolls[i + 1] = r }
-        desired_rolls << good_rolls
-        break
-      end
+  (1..6).each do |current_roll|
+    if dice == 1 && current_roll == sum
+      final_rolls = [[current_roll]]
+    elsif (sum - current_roll) != 0 && dice != 1
+      remaining_dice = dice_rolls((sum - current_roll), (dice - 1))
+      remaining_dice.each { |rolls| final_rolls << rolls.unshift(current_roll) }
     end
-    
-    
   end
-  
-  desired_rolls
+  final_rolls
 end
 
-
-# def dice_roll(sum, dice, rolls = Array.new(dice, 1))
-#   desired_rolls = []
-
-#   if dice != 0 
-#     (1..6).each do |current_roll|
-
-#     ## currently flattening the array so after more than 2 dice it becomes a single array
-#     # need to make sure rolls stay in groups of three
-      
-#       (dice).times do 
-#         # binding.pry
-#         break if sum == 0
-#         other_rolls = dice_roll((sum - current_roll), (dice - 1)).flatten
-#         current_sum = current_roll + other_rolls.sum
-        
-#         if current_sum == sum
-#           desired_rolls << [current_roll] + other_rolls 
-#           break
-#         end
-#       end
-#     end
-#   end
-#   desired_rolls
-# end
-
-# **Algorithm - desired_outcomes**
-# 1. Create all possible outcomes
-#   - create array with dice # of elements
-#   - create recursive method 
-#     - each element (representing a die) starts at 1
-#     - the method increases one element by 1 up to 6
-#     - on each increase it checks if the arrays sum is equal to the desired sum
-#     - if so it adds the dice array to the desired_outcomes array
-    
-
 def rolldice_sum_prob(sum, dice)
-  total_possible_outcomes = dice * 6
-  desired_outcomes = []
-  
-  dice_roll(sum, dice)
-  
+  total_possible_outcomes = 6**dice
+  desired_outcomes = dice_rolls(sum, dice)
+  desired_outcomes.size / total_possible_outcomes.to_f
 end
 
 
@@ -79,9 +28,9 @@ end
 # 5. divide size of array by the total number of possible outcomes
 # 6. return result
 
-p rolldice_sum_prob(7, 2)
-p rolldice_sum_prob(6, 3)
+# p rolldice_sum_prob(7, 2)
+# p rolldice_sum_prob(6, 3)
 
-# p rolldice_sum_prob(11, 2) == 0.0555555555 # or 1/18
-# p rolldice_sum_prob(8, 2) ==  0.13888888889# or 5/36
-# p rolldice_sum_prob(8, 3) == 0.0972222222222  # or 7/72
+p rolldice_sum_prob(11, 2) == 0.05555555555555555 # or 1/18
+p rolldice_sum_prob(8, 2) ==  0.1388888888888889 # or 5/36
+p rolldice_sum_prob(8, 3) == 0.09722222222222222  # or 7/72
---
+++ zoo_patrol.rb
@@ -0,0 +1,82 @@
+=begin
+**Problem**
+Write a function that takes a shuffled list of unique numbers from 1 to n with one element missing (which can be any number including n). Return this missing number.
+
+Note: huge lists will be tested.
+
+Rules:
+Explicit
+  - Input: an array of integers
+  - output: an integer
+  - input array will be a list of numbers from 1 to n with one number missing
+  - the list will be shuffled
+  - return the missing number
+Implicit
+  - all integers (no floats)
+  - if all numbers are in as series the return the next number that would appear
+
+**Examples / Test Cases**
+zoo([1, 3, 4])  ==  2
+zoo([1, 2, 3])  ==  4
+zoo([4, 2, 3])  ==  1
+
+**Data Structures**
+- input array
+- output integer
+
+**Algorithm**
+1. Sort the input array from smallest to largets
+2. Iterate over that array
+  - check to see if the next number is one greater than the current number
+  - if not then return the current number + 1
+  - if the current number is the last element then return it + 1
+=end
+
+require 'pry'
+
+# def zoo(array)
+#   array = array.sort
+#   escaped_number = nil
+  
+#   escaped_number = 1 if array == []
+  
+#   array.each_with_index do |number, index|
+#     if array[0] != 1
+#       escaped_number = 1
+#       break
+#     elsif index == (array.size - 1)
+#       escaped_number = number + 1
+#       break
+#     elsif array[index + 1] != number + 1
+#       escaped_number = number + 1
+#       break
+#     end
+#   end
+#   p escaped_number
+# end
+
+def zoo(array)
+  array = array.sort
+  
+  escaped_number = nil
+
+  if array == [] || array[0] != 1
+    escaped_number = 1
+  else
+    full_range = (1..array[-1]).to_a
+    combined_arrays = array + full_range
+    unique = combined_arrays.select { |int| combined_arrays.count(int) < 2 }
+    if unique == []
+      escaped_number = array[-1] + 1
+    else
+      escaped_number = unique[0]
+    end
+  end  
+  escaped_number
+end
+
+
+p zoo([1, 3, 4])  ==  2
+p zoo([1, 2, 3])  ==  4
+p zoo([4, 2, 3])  ==  1
+# p zoo([])
